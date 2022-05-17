496ef2c9c1073b39b13be966211a76f37330291a
kzok <yes.notyet@gmail.com>
2022-05-17 23:29:42 +0900

    omit styledComponents from next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  /**
-   * @see https://github.com/vercel/next.js/blob/b579a35e83531519f7edfbbdb1b7b3adc493afb4/examples/with-styled-components/next.config.js
-   */
-  compiler: {
-    styledComponents: true,
-  },
   pageExtensions: ["page.tsx"],
 };
 
